3df440bfa2c05f6d1e2e94854c7022acc8335ccb
sudhdkso <waga1009@gmail.com>
2023-11-15 01:21:17 +0900

    feat: 이벤트에 대한 로직을 다루는 eventService 추가

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -3,6 +3,7 @@ package christmas.controller;
 import christmas.domain.Menus;
 import christmas.domain.OrderMenus;
 import christmas.domain.VisitDate;
+import christmas.service.EventService;
 import christmas.util.Parse;
 import christmas.view.InputView;
 import christmas.view.OutputView;
@@ -10,17 +11,19 @@ import christmas.view.OutputView;
 public class EventController {
     private final InputView inputView;
     private final OutputView outputView;
-    private final Menus menus;
+    private final Menus menus = new Menus();
+    private EventService eventService;
 
     public EventController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
-        menus = new Menus();
     }
 
     public void run() {
         VisitDate visitDate = getVisitDate();
         OrderMenus orderMenus = getOrderMenus();
+
+        eventService = new EventService(visitDate, orderMenus);
     }
 
     private VisitDate getVisitDate() {
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -8,11 +8,7 @@ public class InputView {
             + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
     private static final String ASK_ORDER_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-
-    public InputView() {
-
-    }
-
+    
     public String readVisitDate() {
         return readInputData(ASK_VISIT_DATE);
     }
