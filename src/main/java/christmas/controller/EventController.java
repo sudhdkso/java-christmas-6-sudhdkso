8ba781b82f9fd19d96d75e91c3f18a1fe029dddb
sudhdkso <waga1009@gmail.com>
2023-11-14 22:46:16 +0900

    feat: eventController추가

---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,34 @@
+package christmas.controller;
+
+import christmas.domain.Menus;
+import christmas.domain.OrderMenus;
+import christmas.domain.VisitDate;
+import christmas.util.Parse;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Menus menus;
+
+    public EventController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        menus = new Menus();
+    }
+
+    public void run() {
+        VisitDate visitDate = getVisitDate();
+        OrderMenus orderMenus = getOrderMenus();
+    }
+
+    private VisitDate getVisitDate() {
+        return VisitDate.create(Parse.parseInt(inputView.readValidatetDate()));
+    }
+
+    private OrderMenus getOrderMenus() {
+        return OrderMenus.from(inputView.readOrderMenu());
+    }
+
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,13 +1,15 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.util.Validator;
 
 public class InputView {
     private static final String ASK_VISIT_DATE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
             + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
     private static final String ASK_ORDER_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-    public InputView(){
+
+    public InputView() {
 
     }
 
@@ -15,13 +17,14 @@ public class InputView {
         return readInputData(ASK_VISIT_DATE);
     }
 
-    public String readOrderMenu(){
+    public String readOrderMenu() {
         return readInputData(ASK_ORDER_MENU);
     }
 
-    private String readInputData(String message){
+    private String readInputData(String message) {
         System.out.println(message);
         String input = Console.readLine();
+        Validator.validateBlank(input);
         System.out.println(input);
         return input;
     }
