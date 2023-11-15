5826df470d0446a4d19b47323bac4615275cf71a
sudhdkso <waga1009@gmail.com>
2023-11-15 21:57:42 +0900

    fix: 상수명 변경에 따른 오류 해결

--- src/main/java/christmas/util/Validator.java
+++ src/main/java/christmas/util/Validator.java
@@ -5,7 +5,7 @@ import christmas.domain.enums.ErrorMessage;
 public class Validator {
     public static void validateBlank(String input) {
         if (input.isBlank()) {
-            throw new ErrorException(ErrorMessage.INVALID_ORDER_EXCEPTION);
+            throw new ErrorException(ErrorMessage.INVALID_ORDER_RETRY_EXCEPTION);
         }
     }
 }
