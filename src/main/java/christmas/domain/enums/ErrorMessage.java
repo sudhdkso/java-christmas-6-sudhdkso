73825a5e14238140be1c170b127df3f55a975664
sudhdkso <waga1009@gmail.com>
2023-11-15 23:30:28 +0900

    feat: 에러 메세지 추가

--- src/main/java/christmas/domain/enums/ErrorMessage.java
+++ src/main/java/christmas/domain/enums/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     INVALID_DATE_RETRY_EXCEPTION("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     INVALID_DATE_EXCEPTION("유효하지 않은 날짜입니다."),
     INVALID_MENU_EXCEPTION("유효하지 않은 메뉴입니다."),
-    NUMBER_FORMAT_EXCEPTION("올바른 숫자가 아닙니다.");
+    NUMBER_FORMAT_EXCEPTION("올바른 숫자가 아닙니다."),
+    OVERFLOW_INT_RANGE_EXCEPTION("정수범위를 넘어간 입력입니다. 다시 입력해 주세요."),
+    INVALID_INPUT_EXCEPTION("잘못된 입력입니다. 다시 입력해 주세요.");
 
     private final static String ERROR_PREFIX = "[ERROR] ";
     private final String message;
